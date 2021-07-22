76bfe0903e3aa425ca5256ddedfc083aed9bb505
James Fletcher <james@grakn.ai>
2021-07-22 13:31:40 +0100

    BoundConcludableResolvers need to be organised by root and iteration for the purpose of reiteration messages. Also fixed a preexisting bug where there was no protection for calling done too many times.

--- reasoner/ReasonerProducer.java
+++ reasoner/ReasonerProducer.java
@@ -140,7 +140,7 @@ public class ReasonerProducer implements Producer<ConceptMap> {
     private void requestFailed(int iteration) {
         LOG.trace("Failed to find answer to request in iteration: " + iteration);
         if (options.traceInference()) ResolutionTracer.get().finish();
-        if (resolverRegistry.boundConcludableResolvers().size() == 0) {
+        if (resolverRegistry.reiterationQueryRespondents(rootResolver, iteration).size() == 0) {
             finish();
         } else if (!sentReiterationRequests && iteration == this.iteration) {
             sendReiterationRequests();
@@ -148,17 +148,27 @@ public class ReasonerProducer implements Producer<ConceptMap> {
     }
 
     private void finish() {
-        // query is completely terminated
-        done = true;
-        queue.done();
-        required.set(0);
+        if (!done) {
+            // query is completely terminated
+            done = true;
+            queue.done();
+            required.set(0);
+        }
+    }
+
+    private void exception(Throwable e) {
+        if (!done) {
+            done = true;
+            required.set(0);
+            queue.done(e);
+        }
     }
 
     private void sendReiterationRequests() {
         assert reiterationQueryRespondents == null || reiterationQueryRespondents.isEmpty();
         sentReiterationRequests = true;
-        reiterationQueryRespondents = new HashSet<>(resolverRegistry.boundConcludableResolvers());
-        resolverRegistry.boundConcludableResolvers()
+        reiterationQueryRespondents = new HashSet<>(resolverRegistry.reiterationQueryRespondents(rootResolver, iteration));
+        resolverRegistry.reiterationQueryRespondents(rootResolver, iteration)
                 .forEach(res -> res.execute(actor -> actor.receiveReiterationQuery(reiterationRequest)));
     }
 
@@ -174,14 +184,6 @@ public class ReasonerProducer implements Producer<ConceptMap> {
             } else {
                 finish();
             }
-        }
-    }
-
-    private void exception(Throwable e) {
-        if (!done) {
-            done = true;
-            required.set(0);
-            queue.done(e);
         }
     }
 
